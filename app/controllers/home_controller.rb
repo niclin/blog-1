beebd71c6f566d348d12698ae27cadbe47d2045e
GeorgeWebLabs <georgeweblabs@gmail.com>
2015-04-12 15:54:10 +0800

    change home page styles

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,8 @@ class HomeController < ApplicationController
   def index
     @articles = Article.all.order(created_at: :desc)
   end
+
+  def login
+    redirect_to new_user_session_path
+  end
 end
