0a7c66366479de9a7de7f2c8c8e870cc5dfbce09
GeorgeWebLabs <georgeweblabs@gmail.com>
2015-07-03 20:46:34 +0800

    add article title to head title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,7 @@ module ApplicationHelper
   def getCustomStrftime(time)
     return time.strftime('%A, %d %B, %Y')
   end
+  def content_title(subtitle)
+    content_for :title, subtitle + " - George Web Labs"
+  end
 end
