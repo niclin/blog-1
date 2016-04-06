d5f924a45d58cc3884d7f35815683884fd6bd950
GeorgeWebLabs <georgeweblabs@gmail.com>
2016-04-06 12:52:29 +0800

    add portfolio page

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   #end
   root 'home#index'
   get 'about' => 'home#about'
+  get 'portfolio' => 'home#portfolio'
   resources :articles
 
   get 'login' => 'home#login'
